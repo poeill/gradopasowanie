50f2f79978cc09eae9578521924eaed8fda97e2e
krzysztof krzysztof <krzysztof_podczaski@wp.pl>
2017-06-11 17:07:56 +0200

    czasomierz

--- gradopasowanie/Form1.Designer.cs
+++ gradopasowanie/Form1.Designer.cs
@@ -22,12 +22,10 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+       
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label16 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
@@ -45,6 +43,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.czasomierz1 = new System.Windows.Forms.Timer(this.components);
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -90,210 +89,231 @@
             // 
             this.label16.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label16.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label16.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label16.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label16.Location = new System.Drawing.Point(404, 383);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(125, 127);
             this.label16.TabIndex = 15;
-            this.label16.Text = "^";
+            this.label16.Text = "c";
             this.label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label16.Visible = false;
+            this.label16.Click += new System.EventHandler(this.label_Click);
             // 
             // label15
             // 
             this.label15.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label15.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label15.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label15.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label15.Location = new System.Drawing.Point(271, 383);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(125, 127);
             this.label15.TabIndex = 14;
-            this.label15.Text = "$";
+            this.label15.Text = "c";
             this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label15.Visible = false;
+            this.label15.Click += new System.EventHandler(this.label_Click);
             // 
             // label14
             // 
             this.label14.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label14.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label14.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label14.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label14.Location = new System.Drawing.Point(138, 383);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(125, 127);
             this.label14.TabIndex = 13;
-            this.label14.Text = "$";
+            this.label14.Text = "c";
             this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label14.Visible = false;
+            this.label14.Click += new System.EventHandler(this.label_Click);
             // 
             // label13
             // 
             this.label13.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label13.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label13.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label13.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label13.Location = new System.Drawing.Point(5, 383);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(125, 127);
             this.label13.TabIndex = 12;
-            this.label13.Text = "j";
+            this.label13.Text = "c";
             this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label13.Visible = false;
+            this.label13.Click += new System.EventHandler(this.label_Click);
             // 
             // label12
             // 
             this.label12.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label12.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label12.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label12.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label12.Location = new System.Drawing.Point(404, 256);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(125, 125);
             this.label12.TabIndex = 11;
-            this.label12.Text = "%";
+            this.label12.Text = "c";
             this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label12.Visible = false;
+            this.label12.Click += new System.EventHandler(this.label_Click);
             // 
             // label11
             // 
             this.label11.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label11.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label11.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label11.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label11.Location = new System.Drawing.Point(271, 256);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(125, 125);
             this.label11.TabIndex = 10;
-            this.label11.Text = "j";
+            this.label11.Text = "c";
             this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label11.Visible = false;
+            this.label11.Click += new System.EventHandler(this.label_Click);
             // 
             // label10
             // 
             this.label10.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label10.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label10.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label10.Location = new System.Drawing.Point(138, 256);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(125, 125);
             this.label10.TabIndex = 9;
-            this.label10.Text = "%";
+            this.label10.Text = "c";
             this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label10.Visible = false;
+            this.label10.Click += new System.EventHandler(this.label_Click);
             // 
             // label9
             // 
             this.label9.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label9.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label9.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label9.Location = new System.Drawing.Point(5, 256);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(125, 125);
             this.label9.TabIndex = 8;
-            this.label9.Text = "^";
+            this.label9.Text = "c";
             this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label9.Visible = false;
+            this.label9.Click += new System.EventHandler(this.label_Click);
             // 
             // label8
             // 
             this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label8.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label8.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label8.Location = new System.Drawing.Point(404, 129);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(125, 125);
             this.label8.TabIndex = 7;
-            this.label8.Text = "!";
+            this.label8.Text = "c";
             this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label8.Visible = false;
+            this.label8.Click += new System.EventHandler(this.label_Click);
             // 
             // label7
             // 
             this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label7.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label7.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label7.Location = new System.Drawing.Point(271, 129);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(125, 125);
             this.label7.TabIndex = 6;
-            this.label7.Text = "b";
+            this.label7.Text = "c";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label7.Visible = false;
+            this.label7.Click += new System.EventHandler(this.label_Click);
             // 
             // label6
             // 
             this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label6.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label6.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label6.Location = new System.Drawing.Point(138, 129);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(125, 125);
             this.label6.TabIndex = 5;
-            this.label6.Text = "*";
+            this.label6.Text = "c";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label6.Visible = false;
+            this.label6.Click += new System.EventHandler(this.label_Click);
             // 
             // label5
             // 
             this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label5.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label5.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label5.Location = new System.Drawing.Point(5, 129);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(125, 125);
             this.label5.TabIndex = 4;
-            this.label5.Text = "o";
+            this.label5.Text = "c";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label5.Visible = false;
+            this.label5.Click += new System.EventHandler(this.label_Click);
             // 
             // label4
             // 
             this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label4.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label4.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label4.Location = new System.Drawing.Point(404, 2);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(125, 125);
             this.label4.TabIndex = 3;
-            this.label4.Text = "!";
+            this.label4.Text = "c";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.label_Click);
             // 
             // label3
             // 
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label3.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label3.Location = new System.Drawing.Point(271, 2);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(125, 125);
             this.label3.TabIndex = 2;
-            this.label3.Text = "*";
+            this.label3.Text = "c";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label_Click);
             // 
             // label2
             // 
             this.label2.BackColor = System.Drawing.SystemColors.Highlight;
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label2.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label2.Location = new System.Drawing.Point(138, 2);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(125, 125);
             this.label2.TabIndex = 1;
-            this.label2.Text = "b";
+            this.label2.Text = "c";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label_Click);
             // 
             // label1
             // 
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label1.Location = new System.Drawing.Point(5, 2);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(125, 125);
             this.label1.TabIndex = 0;
-            this.label1.Text = "o";
+            this.label1.Text = "c";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label_Click);
+            // 
+            // czasomierz1
+            // 
+            this.czasomierz1.Interval = 750;
+            this.czasomierz1.Tick += new System.EventHandler(this.czasomierz1_Tick);
             // 
             // Form1
             // 
@@ -303,6 +323,7 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "gra_dopasowanie";
+            this.Click += new System.EventHandler(this.label_Click);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -327,6 +348,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer czasomierz1;
     }
 }
 
--- gradopasowanie/Form1.cs
+++ gradopasowanie/Form1.cs
@@ -13,16 +13,23 @@ namespace gradopasowanie
     public partial class Form1 : Form
     {
 
+        Label firstClicked = null;
+
+        Label secondClicked = null;
+
+
         Random random = new Random();
 
         List<string> icons = new List<string>()
-            {
+        {
             "!", "!", "o", "o", "%", "%", "$", "$",
             "^", "^", "b", "b", "*", "*", "j", "j"
         };
+       
+
         private void AssignIconsToSquares()
         {
-           
+
             foreach (Control control in tableLayoutPanel1.Controls)
             {
                 Label iconLabel = control as Label;
@@ -35,14 +42,60 @@ namespace gradopasowanie
                 }
             }
         }
-
-
-
-
         public Form1()
         {
             InitializeComponent();
             AssignIconsToSquares();
         }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+
+            if (czasomierz1.Enabled == true)
+                return;
+                Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    firstClicked.Visible = true;
+                    return;
+                }
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                czasomierz1.Start();
+            }
+
+        }
+
+        private void czasomierz1_Tick(object sender, EventArgs e)
+        {
+            czasomierz1.Stop();
+
+            
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            
+            firstClicked = null;
+            secondClicked = null;
+        }
     }
+
+
+
+
+
+
+
 }
+ 
